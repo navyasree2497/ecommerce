fb749667eeadfaf04027a106eb6fe368e91c18f1
Nvya <navyasree.vgf120@gmail.com>
2022-11-10 21:34:43 +0530

    ProductService

--- src/main/java/com/server/ecommerce/service/ProductService.java
+++ src/main/java/com/server/ecommerce/service/ProductService.java
@@ -1,2 +1,30 @@
-package com.server.ecommerce.service;public class ProductService {
+package com.server.ecommerce.service;
+
+import com.server.ecommerce.entity.Product;
+import com.server.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository repository;
+
+    public Product addProduct(Product product){
+        return repository.save(product);
+    }
+
+    public List<Product> addProducts(List<Product> products){
+        return repository.saveAll(products);
+    }
+
+    public List<Product> getProducts(){
+        return repository.findAll();
+    }
+
+    public Product getProductById(int id){
+        return repository.findById(id).orElse(null);
+    }
 }
