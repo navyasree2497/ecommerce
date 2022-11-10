593570400c638fd9bb4505386785a76207ca7f7d
Nvya <navyasree.vgf120@gmail.com>
2022-11-10 21:37:00 +0530

    ProductRestEndpoints

--- src/main/java/com/server/ecommerce/controller/ProductController.java
+++ src/main/java/com/server/ecommerce/controller/ProductController.java
@@ -1,2 +1,35 @@
-package com.server.ecommerce.controller;public class ProductController {
+package com.server.ecommerce.controller;
+
+import com.server.ecommerce.entity.Product;
+import com.server.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping("/addProduct")
+    public Product addProduct(@RequestBody Product product){
+        return service.addProduct(product);
+    }
+
+    @PostMapping("/addProducts")
+    public List<Product> addProducts(@RequestBody List<Product> products){
+        return service.addProducts(products);
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return service.getProducts();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product findProductById (@PathVariable int id){
+        return service.getProductById(id);
+    }
 }
