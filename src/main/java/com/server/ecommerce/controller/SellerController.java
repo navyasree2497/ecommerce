6716bef5a51f92a35de1b46d4a04221006688781
girisha98 <garikapatigirisha98@gmail.com>
2022-11-25 23:57:23 +0530

    sellers listing by productid

--- src/main/java/com/server/ecommerce/controller/SellerController.java
+++ src/main/java/com/server/ecommerce/controller/SellerController.java
@@ -1,10 +1,13 @@
 package com.server.ecommerce.controller;
 
 import com.server.ecommerce.entity.Seller;
+import com.server.ecommerce.entity.SellerDTO;
 import com.server.ecommerce.service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @RestController
@@ -31,5 +34,24 @@ public class SellerController {
     @GetMapping("/seller/{id}")
     public Seller findSellerById (@PathVariable int id){
         return service.getSellerById(id);
+    }
+
+//    @GetMapping("/products/getSellers/{id}")
+//    @CrossOrigin
+//    public List<SellerDTO> findSellersByProductId(@PathVariable int id){
+//        System.out.println(id);
+//        return service.getSellersByProductId(id);
+//    }
+    @Autowired
+    private EntityManager entityManager;
+    @GetMapping("/products/getSellers/{id}")
+    @CrossOrigin
+    public List<SellerDTO> findSellersByProductId(@PathVariable int id){
+        Query q = entityManager.createNativeQuery("SELECT s.id, s.name, ps.price, ps.discount  FROM sellers s " +
+                "                INNER JOIN product_seller ps " +
+                "                ON ps.seller_id = s.id  " +
+                "                WHERE ps.product_id = ?1", SellerDTO.class);
+        q.setParameter(1,id);
+        return q.getResultList();
     }
 }
